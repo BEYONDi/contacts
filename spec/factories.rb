adcd3ce5ce0936b1529aa98b6cd309741f8d56ff
Nate Biondi <nbiondi@emyth.com>
2015-07-27 14:27:15 -0700

    Added factory. Begin contact model spec.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,9 @@
 FactoryGirl.define do
 
+  factory :john do
+    first_name "John"
+    last_name "Doe"
+    email "john@example.com"
+  end
+
 end
---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Contact do
+  let(:contact) { create(:john) }
+
+  # it{ is_expected.to eq("John")}
+  # it{ is_expected.to have("Doe") }
+  # it{ is_expected.to have("john@example.com") }
+end
