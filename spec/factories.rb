9430021304fea4e443504b451e1c23cf78265d79
Nate Biondi <nbiondi@emyth.com>
2015-07-27 16:02:56 -0700

    Completed first model spec with factory girl

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
 
-  factory :john do
+  factory :john, class: Contact do
     first_name "John"
     last_name "Doe"
     email "john@example.com"
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 describe Contact do
-  let(:contact) { create(:john) }
+  let(:john) { create(:john) }
 
-  it{ should allow_value("John").for(:first_name)}
-  it{ should allow_value("Doe").for(:last_name)}
-  it{ should allow_value("john@example.com").for(:email)}
+  it{ should allow_value("John").for(:first_name) }
+  it{ should allow_value("Doe").for(:last_name) }
+  it{ should allow_value("john@example.com").for(:email) }
+
+  describe 'after create' do
+    it 'Validate saved values' do
+      expect(john.first_name).to eq("John")
+      expect(john.last_name).to eq("Doe")
+      expect(john.email).to eq("john@exmaple.com")
+    end
+  end
 end
