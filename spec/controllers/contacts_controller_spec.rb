c155179a216329574f823af57a0a0f24ad42f662
Nate Biondi <nbiondi@emyth.com>
2015-07-27 16:37:15 -0700

    ensure non nill values for first, last name and email and the test for it.

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 describe ContactsController do
-  describe "GET #index" do
-    it "populates array of contacts"
-    it "renders the :index view"
+  describe 'GET #index' do
+    it 'responds successfully with an HTTP 200 status code:' do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+    it 'render index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+    
+    it 'populates array of contacts'
+
+  end
+  describe 'Get #show' do
+    it 'render the #show view' do
+      get :show, id: create(:john)
+      expect(response).to render_template(:show)
+    end
   end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -6,12 +6,15 @@ describe Contact do
   it{ should allow_value("John").for(:first_name) }
   it{ should allow_value("Doe").for(:last_name) }
   it{ should allow_value("john@example.com").for(:email) }
+  it{ is_expected.to validate_presence_of(:first_name) }
+  it{ is_expected.to validate_presence_of(:last_name) }
+  it{ is_expected.to validate_presence_of(:email) }
 
   describe 'after create' do
     it 'Validate saved values' do
       expect(john.first_name).to eq("John")
       expect(john.last_name).to eq("Doe")
-      expect(john.email).to eq("john@exmaple.com")
+      expect(john.email).to eq("john@example.com")
     end
   end
 end
