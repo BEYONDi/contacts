ddf4c9a904af6000427059c5cdb3b2be636d9cf9
Nate Biondi <nbiondi@emyth.com>
2015-07-30 14:44:15 -0700

    Had to branch this bootstrap mess with a bit of bower chaos mixed in.

--- Gemfile
+++ Gemfile
@@ -1,18 +1,18 @@
 source 'https://rubygems.org'
 
 
-gem 'rails', '4.1.10'
+gem 'rails', '4.2.2'
 gem 'sqlite3'
-gem 'sass-rails', '~> 4.0.3'
+gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
+gem 'sprockets-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 #gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
-# gem 'will_paginate', '~> 3.0.5'
 gem 'kaminari', '~> 0.16.1'
 
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
--- config/application.rb
+++ config/application.rb
@@ -8,17 +8,22 @@ Bundler.require(*Rails.groups)
 
 module Contacts
   class Application < Rails::Application
-    config.generators.jbuilder = false
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # config.generators.jbuilder = false
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.generators do |g|
+      g.template_engine :erb
+      g.jbuilder :false
+    end
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # Bower asset paths
+    root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+      config.sass.load_paths << bower_path
+      config.assets.paths << bower_path
+    end
+
+    # Precompile Bootstrap fonts
+    config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+    # Minimum Sass number precision required by bootstrap-sass
+    ::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
   end
 end
